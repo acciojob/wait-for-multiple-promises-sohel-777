0eac2652af78ea17febf9a12d89abb18c6a41895
SK SOHEL RAHAMAN <sohelrahaman43@gmail.com>
2023-03-23 20:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 let rows=document.querySelector("#output");
 
 	rows.innerHTML=
@@ -6,28 +5,28 @@ let rows=document.querySelector("#output");
 		  <td colspan=2 >Loading...</td>
 		  </tr>`;
 
-let Promise1 = new Promise((resolve,reject)=>{
+let p1 = new Promise((resolve,reject)=>{
 	let time1=Math.random()*2+1;
 	setTimeout(()=>{
 		resolve(time1);
 	},time1*1000);
 });
 
-let Promise2 = new Promise((resolve,reject)=>{
+let p2 = new Promise((resolve,reject)=>{
 	let time2=Math.random()*2+1;
 	setTimeout(()=>{
 		resolve(time2);
 	},time2*1000);
 });
 
-let Promise3 = new Promise((resolve,reject)=>{
+let p3 = new Promise((resolve,reject)=>{
 	let time3=Math.random()*2+1;
 	setTimeout(()=>{
 		resolve(time3);
 	},time3*1000);
 });
 
-Promise.all([Promise1,Promise2,Promise3]).then((result)=>{
+Promise.all([p1,p2,p3]).then((result)=>{
 	let sum=0;
 	rows.innerHTML="";
 	for(var i=0;i<result.length;i++){
